00224194204838bb92cb6fe7f3f014377e2e72be
thuan <235thuan@gmail.com>
2020-06-22 17:56:10 +0700

    khung chưa sửa

--- src/main/java/AppInit.java
+++ src/main/java/AppInit.java
@@ -1,2 +1,38 @@
-public class AppInit {
+//package manageCustomer.Province.config;
+
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.Filter;
+
+
+//lớp khởi tạo ứng dụng
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+//        return new Class[]{AppConfiguration.class,WebSecurityConfig.class};
+//        return new Class[]{WebSecurityConfig.class}; //tao bộ lọc securityfilterchain
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{};
+//        return new Class[0];
+//        return new Class[]{AppConfiguration.class};  //Cấu hình spring web
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return new Filter[]{filter};
+    }
 }

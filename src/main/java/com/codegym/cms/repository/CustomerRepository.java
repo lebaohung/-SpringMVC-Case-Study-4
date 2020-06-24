c555bbf0975b574fc082b20c672451feceb98ec5
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-06-25 01:32:56 +0700

    delete unused files

--- src/main/java/com/codegym/cms/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.codegym.cms.repository;
-//
-//import com.codegym.cms.model.Customer;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface CustomerRepository extends CrudRepository<Customer, Long> {
-//}
